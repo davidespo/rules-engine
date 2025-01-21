fd55095a4082d4477c2d8052426d0a7127849567
David Esposito <david@amplifiersecurity.com>
2025-01-21 09:26:24 -0500

    fix: export functionality

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function hello() {
-  console.log("Hello, world!");
-}
+import {
+  RulesEngine,
+  INSIGHT_IDS_RULE_ID_INDEX,
+  INSIGHT_IDS_VALUE_ID_INDEX,
+} from "./RulesEngine";
+import type { Insight, InsightIds, InsightRule, Matcher } from "./types";
+
+export {
+  RulesEngine,
+  Insight,
+  InsightIds,
+  InsightRule,
+  Matcher,
+  INSIGHT_IDS_RULE_ID_INDEX,
+  INSIGHT_IDS_VALUE_ID_INDEX,
+};
